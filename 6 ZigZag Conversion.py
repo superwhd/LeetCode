9989952ba9a2ee63bc4d0680518f8cf876550844
rrrl8523690 <rrrl8523690@users.noreply.github.com>
2016-08-16 13:28:37 -0700

    upload

--- 6 ZigZag Conversion.py
+++ 6 ZigZag Conversion.py
@@ -1,22 +1,22 @@
-class Solution(object):
-    def convert(self, s, numRows):
+class  Solution(object):
+    def  convert(self, s, numRows):
         """
         :type s: str
         :type numRows: int
         :rtype: str
         """
-        if numRows == 1:
-            return s
-        steps = (1, 0), (-1, 1)
-        n = numRows
-        x = y = 0
-        dir = 0
-        positions = []
-        for ch in s:
+        if  numRows ==  1:
+            return  s
+        steps =  (1, 0), (- 1, 1)
+        n =  numRows
+        x =  y =  0
+        dir =  0
+        positions =  []
+        for  ch in  s:
             positions.append((x, y, ch))
-            x += steps[dir][0]
-            y += steps[dir][1]
-            if x == 0 or x == n - 1:
-                dir = dir ^ 1
-        res = ''.join([tmp[2] for tmp in sorted(positions)])
-        return res
+            x +=  steps[dir][0]
+            y +=  steps[dir][1]
+            if  x ==  0 or  x ==  n -  1:
+                dir =  dir ^  1
+        res =  ''.join([tmp[2] for  tmp in  sorted(positions)])
+        return  res
