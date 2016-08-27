a4ea8dce5688511fbf6e1c2833c13192eeae4ca7
whd <rrrl8523690@users.noreply.github.com>
2016-08-26 23:36:41 -0700

    upload

--- 316 Remove Duplicate Letters.py
+++ 316 Remove Duplicate Letters.py
@@ -5,31 +5,25 @@ class  Solution(object):
         :type s: str
         :rtype: str
         """
-        if  not  s:
-            return  ''
-        st =  [0] *  len(s)
-        now =  0
-        for  i in  xrange(len(s) -  1, - 1, - 1):
-            ch =  s[i]
-            now |=  1 <<  (ord(ch) -  ord('a'))
-            st[i] =  now
-        now =  0
-        i =  0
-        ans =  ''
-        cou =  0
-        all =  st[0]
-        while  i <  len(s) and  now !=  all:
-            j =  i
-            minCh =  - 1 
-            while  j <  len(s) and  (st[i] &  (all ^  now)) ==  (st[j] &  (all ^  now)):
-                ch =  s[j]
-                if  not  (now >>  (ord(ch) -  ord('a')) &  1) and  ((minCh ==  - 1) or  s[minCh] >  s[j]):
-                    minCh =  j 
-                j +=  1
-            ans +=  s[minCh]
-            now |=  1 <<  (ord(s[minCh]) -  ord('a'))
-            i =  minCh +  1 
-        return  ans
+        def  toInt(ch):
+            return  ord(ch) -  ord('a')
+        stack =  []
+        cnt =  [0] *  26
+        for  ch in  s:
+            cnt[toInt(ch)] +=  1
+        vis =  0
+        for  i, ch in  enumerate(s):
+            if  vis >>  toInt(ch) &  1:
+                cnt[toInt(ch)] -=  1
+                continue 
+            while  stack and  stack[- 1] >  ch and  cnt[toInt(stack[- 1])]:
+                vis ^=  1 <<  toInt(stack[- 1])
+                stack.pop()
+            stack.append(ch)
+            vis |=  1 <<  toInt(ch)
+            cnt[toInt(ch)] -=  1
+        return  ''.join(stack)
+            
 
 
-print  Solution().removeDuplicateLetters('abacb')
+
