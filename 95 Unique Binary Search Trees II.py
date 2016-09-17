306321ed8d1b7a3d5b5c49ed755864a457f65bea
whd <rrrl8523690@users.noreply.github.com>
2016-09-17 15:22:19 -0700

    upload

---
+++ 95 Unique Binary Search Trees II.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class  Solution(object):
+    def  generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        def  copyTree(root):
+            if  not  root:
+                return  None
+            copiedRoot =  TreeNode(root.val)
+            copiedRoot.left =  copyTree(root.left)
+            copiedRoot.right =  copyTree(root.right)
+            return  copiedRoot
+            
+        def  dfs(begin, end):
+            if  begin >  end:
+                return  [None]
+            ans =  []
+            for  i in  xrange(begin, end +  1):
+                lefts =  dfs(begin, i -  1)
+                rights =  dfs(i +  1, end)
+                for  left in  lefts:
+                    for  right in  rights:
+                        root =  TreeNode(i)
+                        root.left =  copyTree(left)
+                        root.right =  copyTree(right)
+                        ans.append(root)
+            return  ans
+        
+        if  not  n:
+            return  []
+        return  dfs(1, n)
+
